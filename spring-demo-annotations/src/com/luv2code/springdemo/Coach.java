e5addd80b9d153329b29ce5dd691f18aa9809c57
Nik Ovchinnikov <nvov.nv@gmail.com>
2020-12-22 21:35:25 +0300

    annotation constructor di use

--- spring-demo-annotations/src/com/luv2code/springdemo/Coach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/Coach.java
@@ -2,4 +2,7 @@ package com.luv2code.springdemo;
 
 public interface Coach {
 	public String getDailyWorkout();
+	
+	public String getDailyFortune();
+	
 }
--- spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -1,14 +1,26 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
+	public FortuneService fs;
+	
+	@Autowired
+	public TennisCoach(FortuneService fs) {
+			this.fs = fs;
+	}
 
 	@Override
 	public String getDailyWorkout() {
-		// TODO Auto-generated method stub
 		return "Practice to vowl awfully when you heat";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		
+		return fs.getFortune();
 	}
 
 }
